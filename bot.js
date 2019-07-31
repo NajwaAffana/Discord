faad167f036a0c487f0e561255993daf06f75743
NajwaAffana <53452714+NajwaAffana@users.noreply.github.com>
2019-07-31 21:48:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client(); 
+
+client.on("ready", () => {
+  console.log(client.user.tag + " Ready!");
+  const channel = client.channels.get("606074929254760448");
+  channel.join().then(connection => {
+    console.log("Successfully connected.");
+  }).catch(e => {
+    console.error(e);
+  });
+});
+
+client.login(process.env.TOKEN);
 
