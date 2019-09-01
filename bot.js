1a4b011ccd9f978d1b9fa917b47a40224afe8cd2
NajwaAffana <53452714+NajwaAffana@users.noreply.github.com>
2019-09-01 15:20:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on("ready", () => {
   console.log(client.user.tag + " Ready!");
-  const channel = client.channels.get("613754309036998656");
+  const channel = client.channels.get("613749249968046123");
   channel.join().then(connection => {
     console.log("Successfully connected.");
   }).catch(e => {
